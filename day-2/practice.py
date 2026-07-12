d3dd1a82e825eaaa14f7e167e89c75426de6c3e2
edddddr <endt6342@gmail.com>
2026-07-12 02:12:25 -0900

    receipt NO

--- day-2/practice.py
+++ day-2/practice.py
@@ -1,14 +1,17 @@
 
 
-def temperature_label(temp):
+# def temperature_label(temp):
 
-    if(temp > 15):
-        print("Cold")
-    elif(temp > 15 and temp < 28):
-        print("Warm")
-    else:
-        print("Hot")
+#     if(temp > 15):
+#         print("Cold")
+#     elif(temp > 15 and temp < 28):
+#         print("Warm")
+#     else:
+#         print("Hot")
     
 
 
-temperature_label(10)
+# temperature_label(10)
+
+for n in range(1, 10):
+    print(f"Receipt #{n}")
