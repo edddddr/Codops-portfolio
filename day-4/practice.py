f6358e55ec00bc03d2bea6fc2170b9b21ff4a9ea
edddddr <endt6342@gmail.com>
2026-07-18 23:26:31 -0900

    Account

---
+++ day-4/practice.py
@@ -0,0 +1,15 @@
+class Account():
+    def __init__(self, owner, balance):
+        self.owner = owner
+        self.balance = balance
+
+    def deposite(self, amount):
+        self.balance += amount
+
+
+
+selam = Account("selam", 2000)
+
+print(selam.balance)
+selam.deposite(20)
+print(selam.balance)
