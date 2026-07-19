c5efd66a752e51cbcedbec51b2241d9707a1cc55
edddddr <endt6342@gmail.com>
2026-07-19 00:06:45 -0900

    getter and setter

--- day-4/practice.py
+++ day-4/practice.py
@@ -17,12 +17,29 @@
 
 
 # private and protected
+# class Account:
+#     def __init__(self, balance):
+#         self.__balance = balance # private
+#     def withdraw(self, amount):
+#         if amount > self.__balance:
+#             print("Insufficient funds")
+#             return
+#         self.__balance -= amount
+
+# getter and setter
 class Account:
-    def __init__(self, balance):
-        self.__balance = balance # private
-    def withdraw(self, amount):
-        if amount > self.__balance:
-            print("Insufficient funds")
-            return
-        self.__balance -= amount
+ def __init__(self, balance):
+    self.__balance = balance
+    @property
+    def balance(self): # getter
+        return self.__balance
+    @balance.setter
+    def balance(self, value): # setter with validation
+        if value < 0:
+            raise ValueError("No negative balance")
+        self.__balance = value
+a = Account(1500)
+a.balance # 1500 (runs the getter)
+a.balance = 2000 # ok (runs the setter)
+a.balance = -5 # ValueError
 
