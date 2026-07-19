457ff53bcd788fcf1d33693898a8d9562673802a
edddddr <endt6342@gmail.com>
2026-07-18 23:33:53 -0900

    private and protected

--- day-4/practice.py
+++ day-4/practice.py
@@ -1,15 +1,28 @@
-class Account():
-    def __init__(self, owner, balance):
-        self.owner = owner
-        self.balance = balance
+# class Account():
+#     def __init__(self, owner, balance):
+#         self.owner = owner
+#         self.balance = balance
 
-    def deposite(self, amount):
-        self.balance += amount
+#     def deposite(self, amount):
+#         self.balance += amount
 
 
 
-selam = Account("selam", 2000)
+# selam = Account("selam", 2000)
 
-print(selam.balance)
-selam.deposite(20)
-print(selam.balance)
+# print(selam.balance)
+# selam.deposite(20)
+# print(selam.balance)
+
+
+
+# private and protected
+class Account:
+    def __init__(self, balance):
+        self.__balance = balance # private
+    def withdraw(self, amount):
+        if amount > self.__balance:
+            print("Insufficient funds")
+            return
+        self.__balance -= amount
+
