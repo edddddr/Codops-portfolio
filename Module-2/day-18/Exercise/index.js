8346a815c340e8c89ec68f1d74e811d9247650d7
edddddr <endt6342@gmail.com>
2026-08-13 08:45:46 -0600

    Array in js

---
+++ Module-2/day-18/Exercise/index.js
@@ -0,0 +1,10 @@
+const menu = ['Doro Wat', 'Tibs', 'Shiro'];
+menu[0]; // "Doro Wat" (zero-based)
+menu.length; // 3
+menu[menu.length - 1]; // last item
+menu.push("Firfir"); // add to end
+menu.pop(); // remove from end
+menu.includes("Tibs"); // true
+menu.indexOf("Shiro"); // 2
+
+
