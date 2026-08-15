726a59ef1ca9c76dd994d7c86a1dd4a57f888a5d
edddddr <endt6342@gmail.com>
2026-08-15 00:45:04 -0600

    feat: reading content & value

--- Module-2/day-19/Exercise/script.js
+++ Module-2/day-19/Exercise/script.js
@@ -1,9 +1,20 @@
 // by id — one element
 const list = document.getElementById("list");
+
 // CSS selector — FIRST match
-const title = document.querySelector("h1");
-const first = document.querySelector(".item");
+const h1 = document.querySelector("h1");
+// console.log(h1.textContent);
+
+const li = document.querySelector(".item");
+
+
 // CSS selector — ALL matches (NodeList)
-const items =
-document.querySelectorAll(".item");
-items.length; //
+const items = document.querySelectorAll(".item");
+items.length; // 2
+
+const qty = document.querySelector("#qty");
+// console.log(qty.value)
+
+
+// console.log(li.getAttribute("Class"))
+console.log(li.dataset.id)
