5e0c6f4df7af6b5762902dd1c3e667ef98d56465
edddddr <endt6342@gmail.com>
2026-08-16 12:39:51 -0600

    feat: callbacks

--- Module-2/day-20/Exercise/script.js
+++ Module-2/day-20/Exercise/script.js
@@ -5,3 +5,15 @@ console.log("3 — food is ready");
 }, 2000);
 console.log("2 — serve next customer");
 // prints: 1, 2, then 3
+
+
+// "call this function when you are done"
+function getOrder(id, callback) {
+    setTimeout(() => {
+        callback({ id, total: 240 }); // ETB
+        }, 1000);
+}
+    
+getOrder(7, (order) => {
+    console.log(order.total);
+});
