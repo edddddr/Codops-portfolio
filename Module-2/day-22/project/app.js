639c75da557432bf21c9154d1d78577fe6cc8b68
edddddr <endt6342@gmail.com>
2026-08-17 02:30:51 -0600

    feat: frontend

---
+++ Module-2/day-22/project/app.js
@@ -0,0 +1,14 @@
+//  https://v6.exchangerate-api.com/v6/ef9f9608a5ebc9655c674645/latest/USD
+
+// {
+//     "result": "success",
+//     "base_code": "ETB",
+//     "rates": {
+//         "USD": 0.0177,
+//         "EUR": 0.0164,
+//         "KES": 2.29,
+//         "GBP": 0.0139
+//     }
+// }
+
+
