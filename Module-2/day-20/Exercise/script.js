6627a0b36d4e1ee253101048daf25329e7b05c36
edddddr <endt6342@gmail.com>
2026-08-18 12:22:54 -0600

    feat: try catch final

--- Module-2/day-20/Exercise/script.js
+++ Module-2/day-20/Exercise/script.js
@@ -21,7 +21,18 @@ getOrder(7, (order) => {
 
 const order = new Promise((resolve, reject) => {
     const ok = true;
-    
+
     if (ok) resolve({ id: 7, total: 240 });
     else reject(new Error("kitchen closed"));
+});
+
+getOrder(7)
+.then(order => {
+console.log(order.total); // on success
+})
+.catch(err => {
+console.log(err.message); // on failure
+})
+.finally(() => {
+hideSpinner(); // always runs
 });
