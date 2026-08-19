3de5414c2bc980d618dc62491257a883647e52cf
edddddr <endt6342@gmail.com>
2026-08-19 02:36:40 -0600

    feat: redering the data

---
+++ Module-2/day-23/Project/script.js
@@ -0,0 +1,21 @@
+const MENU = [
+      { id: '1', name: 'Special Kitfo', category: 'Mains', price: 450, desc: 'Freshly minced beef seasoned with mitmita and niter kibbeh.', img: '🥩' },
+      { id: '2', name: 'Yetsom Beyaynetu', category: 'Fasting', price: 280, desc: 'Assorted lentils, chickpeas, and vegetable stews served on Injera.', img: '🫓' },
+      { id: '3', name: 'Doro Wat', category: 'Mains', price: 550, desc: 'Traditional slow-cooked chicken stew with boiled eggs.', img: '🍗' },
+      { id: '4', name: 'Shiro Tegabino', category: 'Fasting', price: 220, desc: 'Rich chickpea flour stew served boiling hot in a clay pot.', img: '🍲' },
+      { id: '5', name: 'Bedele Special Beer', category: 'Drinks', price: 90, desc: 'Chilled premium Ethiopian lager (330ml).', img: '🍺' },
+      { id: '6', name: 'Fresh Tej (Honey Wine)', category: 'Drinks', price: 150, desc: 'Traditional house-fermented honey wine.', img: '🍯' }
+    ];
+
+
+
+const dishes = document.getElementById("dish-grid")
+
+
+function render(){
+
+
+}
+    
+
+render()
