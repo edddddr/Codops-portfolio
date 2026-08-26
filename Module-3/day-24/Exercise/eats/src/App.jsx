01d4c8d1efd0799c07d7d74276adc9461222ae7e
edddddr <endt6342@gmail.com>
2026-08-26 07:15:40 -0600

    props are done

--- Module-3/day-24/Exercise/eats/src/App.jsx
+++ Module-3/day-24/Exercise/eats/src/App.jsx
@@ -1,14 +1,30 @@
 import "./App.css";
-import Header from "./Home.jsx";
-import Menu from "./Menu.jsx";
+import Header from "./Components/Header/Header.jsx";
+import Main from "./Components/Main/Main.jsx";
+import Footer from "./Components/Footer/Footer.jsx";
 
 function App() {
   return (
     <div>
       <Header />
-      <p>Order great food across Addis.</p>
-      <Menu />
+      <Main />
+      <Footer />
     </div>
   );
 }
 export default App;
+
+// import "./App.css";
+// import Header from "./Home.jsx";
+// import Menu from "./Menu.jsx";
+
+// function App() {
+//   return (
+//     <div>
+//       <Header />
+//       <p>Order great food across Addis.</p>
+//       <Menu />
+//     </div>
+//   );
+// }
+// export default App;
