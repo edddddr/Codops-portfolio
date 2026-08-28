336b49d7fee8dcea8f1cce6e4d7cbe0a2d4a446a
edddddr <endt6342@gmail.com>
2026-08-28 02:34:17 -0600

    feat: input event listner and state managment

--- Module-3/day-26/Project/eats/src/State/State.jsx
+++ Module-3/day-26/Project/eats/src/State/State.jsx
@@ -2,13 +2,40 @@ import { useState } from "react";
 
 function State() {
   let [count, setCount] = useState(0);
+  let [name, setName] = useState("");
+  let [phone, setPhone] = useState("");
 
   const handleCount = () => setCount(count + 1);
+
+  const [form, setForm] = useState({
+    name: "",
+    phone: "",
+  });
+
+  function handleChange(e) {
+    const { name, value } = e.target;
+    setForm({ ...form, [name]: value }); // copy, then override
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(form.name, form.phone);
+  };
 
   return (
     <>
       <h1>{count}</h1>
       <button onClick={handleCount}>+</button>
+
+      <form onSubmit={handleSubmit}>
+        <label>Name: </label>
+        <input name="name" value={form.name} onChange={handleChange} />
+        <br />
+        <label>Phone: </label>
+        <input name="phone" value={form.phone} onChange={handleChange} />
+        <br />
+        <button type="submit">Submit</button>
+      </form>
     </>
   );
 }
